7bca934b0d5387e39b6c24ebb0ca24986600f934
Matthew Kilgore <tankerkiller125@gmail.com>
2021-05-16 11:22:29 -0400

    Use Quasar FrontEnd (May be replaced)

--- Staat/WebApp/src/App.vue
+++ Staat/WebApp/src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'LayoutDefault',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    HelloWorld
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
     }
   }
 }
-</style>
+</script>
--- Staat/WebApp/src/main.ts
+++ Staat/WebApp/src/main.ts
@@ -3,5 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { Quasar } from 'quasar'
+// @ts-ignore
+import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
